ef4b2424169f2d82b4fa8d14cd1c3dd305b820ab
Pasto <rpastorelle@yahoo.com>
2015-09-16 20:51:25 -0400

    Homepage content.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>SuperRhino.net</title>
+        <meta name="description" content="This is SuperRhino.net – What's here? Just some things...">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta property="og:image" content="http://superrhino.net/rpp/skyClouds.jpg">
+
+    </head>
+    <body style="margin:0;padding:0;">
+        <!--[if lt IE 9]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <?php if ( isset($_GET['text']) ): ?>
+
+        <!-- Add your site or application content here -->
+        <h1>Commissioner.club!</h1>
+        <h2>The swankiest fantasy football club on the web <a href="/">Commissioner.Club</a></h2>
+        <p>What's here? Just some things:</p>
+        <ul>
+            <li><a href="http://games.espn.go.com/ffl/leagueoffice?leagueId=388206">Official League Page – ESPN</a>
+            <li><a href="https://tackk.com/the-fantasy-commission">The Fantasy Commission League News – Tackk</a>
+            <li><a href="https://tackk.com/tfc-records">TFC All-time Record Book</a>
+        </ul>
+
+        <?php else: ?>
+
+        <div id="tackk-1dfjzs" style="margin:0;padding:0;overflow:hidden;width:100%;height:2085px">
+            <iframe id="tackk-1dfjzs-frame" src="https://tackk.com/1dfjzs/embed" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+        </div>
+
+        <?php endif; ?>
+
+    </body>
+</html>
