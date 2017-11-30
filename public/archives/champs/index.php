e80c0b9bce57bedb15a12f93db5928e97fa75aba
Ryan Pastorelle <rpastorelle@yahoo.com>
2017-11-29 23:22:42 -0600

    Move keepers; update links

--- public/archives/champs/index.php
+++ public/archives/champs/index.php
@@ -14,12 +14,12 @@
         <h1>Commissioner.club!</h1>
         <h2>Hall of Champions</h2>
 
-        <img src="./champs/2016-kfc.jpg" alt="2016 - KFC Karaokee" class="champshot" />
-        <img src="./champs/2015-slicknicks.jpg" alt="2015 - Slick Nicholas" class="champshot" />
-        <img src="./champs/2014-beer.jpg" alt="2014 - Team BEER" class="champshot" />
-        <img src="./champs/2013-emo.jpg" alt="2013 - Von Bologna Hates Emo City" class="champshot" />
-        <img src="./champs/2012-skyclouds.jpg" alt="2012 Sky Clouds" class="champshot" />
-        <img src="./champs/2011-falcopunch.jpg" alt="2011 Falco Punch" class="champshot" />
-        <img src="./champs/2010-beer.jpg" alt="2010 Team BEER" class="champshot" />
+        <img src="./2016-kfc.jpg" alt="2016 - KFC Karaokee" class="champshot" />
+        <img src="./2015-slicknicks.jpg" alt="2015 - Slick Nicholas" class="champshot" />
+        <img src="./2014-beer.jpg" alt="2014 - Team BEER" class="champshot" />
+        <img src="./2013-emo.jpg" alt="2013 - Von Bologna Hates Emo City" class="champshot" />
+        <img src="./2012-skyclouds.jpg" alt="2012 Sky Clouds" class="champshot" />
+        <img src="./2011-falcopunch.jpg" alt="2011 Falco Punch" class="champshot" />
+        <img src="./2010-beer.jpg" alt="2010 Team BEER" class="champshot" />
     </body>
 </html>
